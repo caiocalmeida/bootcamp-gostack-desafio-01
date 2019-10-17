a5a683e4af1c3f9e87c4bb22a93d541fcf925ee4
Caio Almeida <40412377+caiocva@users.noreply.github.com>
2019-10-17 00:52:00 -0300

    Add CRUD to the API

--- src/index.js
+++ src/index.js
@@ -1,2 +1,54 @@
 const express = require('express');
 const server = express();
+server.use(express.json());
+
+class Project {
+  constructor(id, title) {
+    this.id = id;
+    this.title = title;
+    this.tasks = [];
+  }
+}
+
+const projects = [];
+
+server.post('/projects', (req, res) => {
+  const { id, title } = req.body;
+
+  const project = new Project(id, title);
+
+  projects.push(project);
+
+  return res.status(201).send('Project created.');
+});
+
+server.get('/projects', (req, res) => {
+  return res.json(projects);
+});
+
+server.put('/projects', (req, res) => {
+  const { id, title } = req.body;
+
+  projects[id].title = title;
+
+  return res.send('Project updated.');
+});
+
+server.delete('/projects/:index', (req, res) => {
+  const { index } = req.params;
+
+  projects.splice(index, 1);
+
+  return res.send('Project deleted.');
+});
+
+server.post('/projects/:id/tasks', (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+
+  projects[id].tasks.push(title);
+
+  return res.send('Project updated');
+});
+
+server.listen(3000);
